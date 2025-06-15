d4f6543283c372472470cb050575f83358c53eac
Miguel <perezcontrerasmiguelalejandro@gmail.com>
2025-06-14 19:02:50 -0700

    [ADD] Modelo contenedor

--- src/main/java/com/upsin/embarcaciones_poo/modelo/Contenedor.java
+++ src/main/java/com/upsin/embarcaciones_poo/modelo/Contenedor.java
@@ -1,4 +1,20 @@
 package com.upsin.embarcaciones_poo.modelo;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Contenedor {
+    @Id
+    @GeneratedValue
+    private Integer idContenedor;
+    private Integer idEmpresa;
+    private Double unidadMedida;
+    private Double pesoTotal;
+    private String observaciones;
 }
