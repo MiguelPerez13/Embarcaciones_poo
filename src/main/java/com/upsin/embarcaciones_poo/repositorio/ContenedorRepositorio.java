349c1836764c5739269bed64a30d7a45963b3019
Miguel <perezcontrerasmiguelalejandro@gmail.com>
2025-06-14 19:02:50 -0700

    [ADD] Modelo contenedor

--- src/main/java/com/upsin/embarcaciones_poo/repositorio/ContenedorRepositorio.java
+++ src/main/java/com/upsin/embarcaciones_poo/repositorio/ContenedorRepositorio.java
@@ -1,4 +1,9 @@
 package com.upsin.embarcaciones_poo.repositorio;
 
-public class ContenedorRepositorio {
+import com.upsin.embarcaciones_poo.modelo.Contenedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ContenedorRepositorio extends JpaRepository<Contenedor,Integer>{
+    
 }
